ea13df5d4caa7ede9804500f1e0324524b812ebc
amoldalwai <amoldalwaiyt@gamil.com>
2020-09-04 17:23:32 +0530

    Updates

---
+++ precache-manifest.420ea1d73758156246645c5656503a35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7696bde764a017a8332d",
+    "url": "/pokedexapp/static/css/main.bd359a3f.chunk.css"
+  },
+  {
+    "revision": "7696bde764a017a8332d",
+    "url": "/pokedexapp/static/js/main.298a13e3.chunk.js"
+  },
+  {
+    "revision": "124b3e85a02d0127e90a",
+    "url": "/pokedexapp/static/js/runtime~main.1546d1bd.js"
+  },
+  {
+    "revision": "dfc0ed9351d6e5bcae44",
+    "url": "/pokedexapp/static/js/2.e622cd98.chunk.js"
+  },
+  {
+    "revision": "099bc98a593299c970d2115becdd4d29",
+    "url": "/pokedexapp/index.html"
+  }
+];
